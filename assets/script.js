13332ba54c4af789f002fe918e17776bc94c1163
grey cashmere <dobsonemily@gmail.com>
2021-03-07 19:06:20 -0700

    fixing bugs

--- assets/script.js
+++ assets/script.js
@@ -16,39 +16,39 @@ var hour16 = $("#hour-16"). text();
 var hour17 = $("#hour-17"). text(); 
 
 
-//Timeblocks are set for standard business hours
-for (let i =0; i < 8; i++){
-    $timeBlock.append(buildTimeBlock(hour))
-    }
+// //Timeblocks are set for standard business hours
+// //for (let i =0; i < 8; i++){
+//     $timeBlock.append(buildTimeBlock(hour))
+//     }
 
 
-//Timeblocks are color coded to indicate whether it is in the past, present, or future
+// //Timeblocks are color coded to indicate whether it is in the past, present, or future
 
-function buildTimeBlock(hour) {
-    var background; 
-    if(currentHour===hour){
-      background= $timeBlock.addClass
-      ("present")
-    }else if( currentHour < hour){
-  background ="past";
-  } else {
-      background= "future"
-    }
-  }
+// function buildTimeBlock(hour) {
+//     var background; 
+//     if(currentHour===hour){
+//       background= $timeBlock.addClass
+//       ("present")
+//     }else if( currentHour < hour){
+//   background ="past";
+//   } else {
+//       background= "future"
+//     }
+//   }
 
-  //when you click a timeblock I can enter an event
+//   //when you click a timeblock I can enter an event
 
 
-//when you click the save button for that timeblock the text is saved to local storage
-$("saveBtn").on("click", function(e){
-    //prevents refresh
-    e.preventDefault ();
-    //if the input of description is nothing(less than 1)return
-    var description= document.querySelectorAll(".description").textContent;
-    if(description.length < 1 )return;
-    //the text of the description var is equal to the input of the description
+// //when you click the save button for that timeblock the text is saved to local storage
+// $("saveBtn").on("click", function(e){
+//     //prevents refresh
+//     e.preventDefault ();
+//     //if the input of description is nothing(less than 1)return
+//     var description= document.querySelectorAll(".description").textContent;
+//     if(description.length < 1 )return;
+//     //the text of the description var is equal to the input of the description
     
-    //Save the list to localStorage
-    localStorage.setItem("description",description); 
-    //function for each desciption
-    }
+//     //Save the list to localStorage
+//     localStorage.setItem("description",description); 
+//     //function for each desciption
+//     }
